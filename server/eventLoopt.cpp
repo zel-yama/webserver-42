5c5858cf93d0287fe0fa41612dd5c68086ab12d3
zel-yama <zakariayamani123@gmail.com>
2025-12-03 15:53:48 +0100

    some code

--- server/eventLoopt.cpp
+++ server/eventLoopt.cpp
@@ -8,18 +8,22 @@ int readRequest(int fd){
     char buffer[1280] ;
     int i = 0;
     string str;
-    while(( i = read(fd, buffer, 1280)) > 0){
-    buffer[i] = '\0';
-	//cout << buffer << endl;
-    buff.push_back(buffer);
+    while(( i = recv(fd, buffer, 1280, MSG_DONTWAIT )) > 0){
+        buffer[i] = '\0';
+
+        buff.push_back(buffer);
     }
     for(vector<string>::iterator it = buff.begin(); it != buff.end(); it++){
-        if (!str.compare("\n\r\n"))
-            return 1;
+        str = *it;
         cout << *it << endl;
+        if (strstr(str.c_str(), "\r\n\r\n"))
+            return 1;
     }
     return 1;
 }
+// void acceptAddClient(Config &data, int FdEP){
+    
+// }
 int creatEpoll( maptype config){
 
     int fdEp;
@@ -28,12 +32,9 @@ int creatEpoll( maptype config){
         cerr << "error in creating epoll " << errno << endl;
     }
     for(ConfigIter it = config.begin(); it != config.end(); it++)
-   {
-        
+   { 
         Server *serv = dynamic_cast<Server*>(it->second);
-
        addSockettoEpoll(fdEp, serv->data);
-
    }
     return fdEp;
 }
@@ -64,22 +65,24 @@ void eventLoop(maptype config ){
                     config.insert(pair<int,Config *>(newClient.fd, &newClient));
                     cout << "accept " << newClient.fd << " from server " << serv->fd << endl; 
                     addSockettoEpoll(fdEp,newClient.data);                    
-                    break;
+                    continue;
                 }
                 if (config.at(events[i].data.fd)->name == "Client")
                 {
                     Cli = dynamic_cast<Client *>(config.at(events[i].data.fd));
-		    
                     if (readRequest(Cli->fd) == 1)
                     {
                         Cli->data.events = EPOLLOUT;
-                        cout << "listen to send response" <<endl;
+                        continue;
                     }
                 }
             }
             if (events[i].data.fd & EPOLLOUT){
-		    
-                write(events[i].data.fd, "yes this is reponse",47);  
+                if(send(events[i].data.fd, "yes this is reponse",47, MSG_DONTWAIT) == -1){
+                  cerr << "error in send operatio" << endl;
+                   continue;
+                } 
+                 close(events[i].data.fd);
             }
         }
     }

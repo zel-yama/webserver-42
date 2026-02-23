7683141c73d34271b2638e4ec33de848ff928239
Zakaria El Yamani <zel-yama@e2r10p4.1337.ma>
2026-02-23 14:51:19 +0000

    just redo some things

--- server/serverSetup/readRequest.cpp
+++ server/serverSetup/readRequest.cpp
@@ -57,7 +57,7 @@ void readRequest(maptype &data,  int fd,  Client &connect, RequestParser *parser
     int readResult = myread(connect, parser->buffer[fd]);
     
     printf("[%s] buffer \n",parser->buffer[fd].c_str() );
-    if (readResult < 0) {
+    if (readResult <= 0) {
         std::cout << "Client " << fd << " closed connection (read 0 bytes)" << std::endl;
         connect.requestFinish = false;
         deleteClient(data, fd, connect.fdEp);
